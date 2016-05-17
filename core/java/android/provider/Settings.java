c5cb2bc283ed5659cecef7338a94ad6dd7f1098a
David <davidlglozo@gmail.com>
2016-05-17 22:32:37 +0200

    Hide supersu icon statusbar[1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3722,6 +3722,11 @@ public final class Settings {
 
 
         /**
+         * @hide
+         */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
