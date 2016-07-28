dff8dedbc9e5cd22a6d432489e29dcc5dbc4f082
Alexander Martinz <eviscerationls@gmail.com>
2016-07-28 13:57:44 +0200

    [2/2] base: cm custom boot dexopt UI
    * Pass app info and number of installed packages to boot message UI
    * Ui by Asher and Joey, based on Alexander's previous work
    
    Change-Id: I9ec9d0cb0e20a9bac73e126f6b6f3965400f05e7

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -6511,9 +6511,9 @@ public class PackageManagerService extends IPackageManager.Stub {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
         try {
-            ActivityManagerNative.getDefault().showBootMessage(
-                    mContext.getResources().getString(R.string.android_upgrading_apk,
-                            curr, total), true);
+            ActivityManagerNative.getDefault().updateBootProgress(
+                    IActivityManager.BOOT_STAGE_PREPARING_APPS,
+                    pkg.applicationInfo, curr, total, true);
         } catch (RemoteException e) {
         }
         PackageParser.Package p = pkg;
